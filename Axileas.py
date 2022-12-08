6edffa3b766aed56027d8f928255cd1b836413be
Axileas <115211756+Axileaszervos@users.noreply.github.com>
2022-12-08 23:00:22 +0200

    Add files via upload

---
+++ Axileas.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+L = []
+for i in range(10):
+        L.append(i)
+        print(L)
---
+++ PythonCompiler.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/bash
+
+# create results.txt
+touch results.txt
+
+#Create a new Python file
+echo "create a new file, type your filename:"
+read newfilename
+echo "Congratulations, you have created a new python file!"
+
+#!/usr/bin/env python in the first line of file
+echo "#!/usr/bin/env python" | cat >> $newfilename
+chmod +x ./$newfilename
+
+echo "Would you like to compile your pogramm:?"
+read case
+
+if [ "$case" = "yes" ];
+then
+echo "compiling $newfilename... "
+sleep 1
+elif [ "$case" = "no" ];
+then
+echo "end"
+fi
+
+#if the file is compiled, the result of programm will overwrite in the results.txt
+if python $newfilename; then
+ ./$newfilename | cat >> results.txt
+fi
+
+
+
